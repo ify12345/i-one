06dd96f99c2b98915c85a5b358a4fab3e1d02b40
Nwakodo Ifeanyi <46559372+ify12345@users.noreply.github.com>
2025-05-20 17:40:42 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,14 @@ import tailwindScrollbar from 'tailwind-scrollbar'
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-     primary: '#00FF94',
-     primaryLight: '#8F8F8F',
-     stroke:'#E2E8F0',
-     success:'#4DB675',
-     danger:'#ff0011'
+    extend: {
+      colors: {
+       primary: '#00FF94',
+       primaryLight: '#8F8F8F',
+       stroke:'#E2E8F0',
+       success:'#4DB675',
+       danger:'#ff0011'
+      },
     },
   },
   plugins: [
